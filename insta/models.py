7a4526f9c603bb33f5d22333fabfe737cd00920a
Clare Richards <claremonte53gmail.com>
2020-06-04 09:54:56 +0300

    added profile model

--- insta/models.py
+++ insta/models.py
@@ -33,22 +33,29 @@ class Image(models.Model):
         
         return self.image_name
 
-class Comments(models.Model):
-    comment = models.CharField(max_length=100,blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    image = models.ForeignKey(Image,on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='images')
+    bio = models.TextField(max_length=100)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     
-    @classmethod
-    def get_comments(cls,id):
-        comments = cls.objects.filter(image__id=id)
-        return comments
+
     
-    def save_comment(self):
-        self.save()
     
     def __str__(self):
-
-        return self.comment
+        
+        return self.profile_photo.url
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod   
+    def update_bio(cls,id,new_bio):
+        cls.objects.filter(pk = id).update(bio=new_bio)
+        new_bio_object = cls.objects.get(bio = new_bio)
+        new_bio = new_bio_object.bio
+        return new_bio
 
 
