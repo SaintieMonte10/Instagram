ec61fd033512b8914cb89e96e2a361de1d8ff02a
Clare Richards <claremonte53gmail.com>
2020-06-04 10:01:29 +0300

    testings

--- insta/models.py
+++ insta/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from vote.models import VoteModel
 
-# Create your models here.
+#create your models here
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=50)
     image_caption = models.CharField(max_length=50)
     likes = models.PositiveIntegerField(default=0)
-    ser = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     
     def save_image(self):
         self.save()
@@ -32,7 +32,24 @@ class Image(models.Model):
     def __str__(self):
         
         return self.image_name
-
+    
+class Comments(models.Model):
+    comment = models.CharField(max_length=100,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image__id=id)
+        return comments
+    
+    def save_comment(self):
+        self.save()
+    
+    def __str__(self):
+        
+        return self.comment
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='images')
     bio = models.TextField(max_length=100)
@@ -57,5 +74,3 @@ class Profile(models.Model):
         new_bio_object = cls.objects.get(bio = new_bio)
         new_bio = new_bio_object.bio
         return new_bio
-
-
--- insta/tests.py
+++ insta/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from . models import *
 
-# Create your tests here.
 class ImageTestClass(TestCase):
     '''
     This is a class that perform unnittest  behaviour on the Image Model.
@@ -9,7 +8,7 @@ class ImageTestClass(TestCase):
     
     def setUp(self):
         
-        self.image_one = Image(image='images/lagoon.jpeg',image_name='dan', image_caption='lacasa de papel',likes=40, id=1)
+        self.image_one = Image(image='images/lagoon.jpeg',image_name='dan', image_caption='lacasa de papel',likes=40, id=1,user_id=3)
         
     def test_instance(self):
         Image.objects.all().delete()
@@ -34,4 +33,41 @@ class ImageTestClass(TestCase):
         self.assertEqual(done,new_caption)
         
     def tearDown(self):
-        Image.objects.all().delete()
+        Image.objects.all().delete()   
+    
+       
+        
+        
+class ProfileTestClass(TestCase):
+    
+    '''
+    This is a class that perform unnittest  behaviour on the Profile Model.
+    '''
+    
+    def setUp(self):
+        self.profile_one = Profile(profile_photo='images/mine.jpg',bio='Currently doing datascience in moringa',user_id=3)
+        
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_one,Profile)) 
+
+    def test_save_method(self):
+        
+        self.profile_one.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile_one.save_profile()
+        self.profile_one.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) is 0)
+        
+    def test_update_method(self):
+        self.profile_one.save_profile()
+        new_bio = '# thursday speaker'
+        done = self.profile_one.update_bio(self.profile_one.pk,new_bio)
+        self.assertEqual(done,new_bio)
+        
+    def tearDown(self):
+        Profile.objects.all().delete()
