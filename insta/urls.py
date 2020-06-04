8bff0853852b9259ec46f3ba2db41d8789690fca
Clare Richards <claremonte53gmail.com>
2020-06-04 08:45:34 +0300

    installed tinmyce

--- insta/urls.py
+++ insta/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
+    url(r'^$',views.home,name='home'),
+    url(r'^image/$', views.image_upload,name='upload'),
 ]
