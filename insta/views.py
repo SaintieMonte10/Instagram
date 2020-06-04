06da65386db1fa1bff053841024dd979f2f3a125
Clare Richards <claremonte53gmail.com>
2020-06-04 09:17:32 +0300

    added votes.managers

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse
+from . forms import ImageUploadForm,ImageProfileForm,CommentForm
+from .models import *
+from django.contrib.auth.decorators import login_required
+from vote.managers import  VotableManager
+from django.contrib.auth.models import User
+votes = VotableManager()
 
 # Create your views here.
 def home(request):
