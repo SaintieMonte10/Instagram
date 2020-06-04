c763ba9c88cab7be0a1d68ef10c14b25f4675886
Clare Richards <claremonte53gmail.com>
2020-06-04 09:51:42 +0300

    updated model imports

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,7 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from vote.models import VoteModel
 
 # Create your models here.
 class Image(models.Model):
@@ -6,7 +9,7 @@ class Image(models.Model):
     image_name = models.CharField(max_length=50)
     image_caption = models.CharField(max_length=50)
     likes = models.PositiveIntegerField(default=0)
-    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+    ser = models.ForeignKey(User,on_delete=models.CASCADE)
     
     def save_image(self):
         self.save()
